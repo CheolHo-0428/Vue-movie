544edf2ba1c79aa10096ad2a2aa7bdd6ae9aff40
CK-0428 <againnr1@gmail.com>
2021-10-13 20:59:30 +0900

    Promise와 Async Await 사용 패턴

--- async.js
+++ async.js
@@ -1,102 +1,186 @@
-function a () {
+let a = () => {
     console.log('a')
 }
-function b () {
+
+let b = () => {
     console.log('b')
 }
 
-a()
-b()
-
-function a() {
-    setTimeout(function () {
-        console.log('a')
-    }, 1000)
-}
-
-function b() {
-    console.log('b')
-}
-
-a()
-b()
-
-
-function a(cb) {
-    setTimeout(function () {
-        console.log('a')
-        cb()
-    }, 1000)
-}
-
-function b() {
-    console.log('b')
-}
-
-a(function () {
-    b()
-})
+a() 
+b() 
 // a
 // b
 
-function a(cb) {
+function a () {
     setTimeout(function () {
         console.log('a')
-        cb()
     }, 1000)
 }
 
-function b(cb) {
-    setTimeout(function () {
-        console.log('b')
-        cb()
-    }, 1000)
+let b = () => {
+    console.log(b)
 }
 
-function c(cb) {
-    setTimeout(function () {
-        console.log('c')
+a()
+b()
+
+//b
+//a
+
+function a (cb) {
+    setTimeout(() => {
+        console.log('a')
         cb()
-    }, 1000)
+    }, 1000);
 }
 
-function d(cb) {
-    setTimeout(function () {
-        console.log('d')
-        cb()
-    }, 1000)
+let b = () => {
+  console.log('b')
 }
 
-a(function(){
-    b(function(){
-        c(function(){
-            d()
-        })
+a(function() {
+  b()
+})
+//a
+//b
+
+
+
+
+
+function a (cb) {
+  setTimeout(() => {
+    console.log('a')
+    cb()
+  }, 1000)
+}
+
+function b (cb) {
+  setTimeout(() => {
+    console.log('b')
+    cb()
+  }, 1000)
+}
+
+function c (cb) {
+  setTimeout(() => {
+    console.log('c')
+    cb()
+  }, 1000)
+}
+
+function d (cb) {
+  setTimeout(() => {
+    console.log('d')
+    cb()
+  }, 1000)
+}
+
+a(function () {
+  b(function () {
+    c(function () {
+      d()
     })
+  })
 })
 
 
-function a(){
-    return new Promise(resolve => {
-        setTimeout(() => {
-            console.log('a')
-            resolve()
-        }, 1000)
-    })
+
+
+
+
+
+
+function a () {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('a')
+      resolve()
+    }, 1000);
+  })
 }
 
-function b(){
-    return new Promise(resolve => {
-        setTimeout(() => {
-            console.log('b')
-            resolve()
-        }, 1000)
-    })
+function b () {
+ return new Promise(resolve => {
+  setTimeout(()=>{
+    console.log('b')
+    resolve()
+  }, 1000)  
+ }) 
 }
 
-a().then(() => {
-   return b()
-}).then(()=>{
-    c()
-}) 
+function c () {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('c')
+      resolve()
+    })   
+  })
+}
 
+function d () {
+  return new Promise(resolve => {
+    setTimeout((resolve)=>{
+      console.log('d')
+      resolve()
+    }, 1000)   
+  })
+}
+
+a()
+  .then(() => b())
+  .then(() => c())
+  .then(() => d())
+
+async function async() {
+  await a()
+  await b()
+  await c()
+  d()
+  console.log('done')
+}
+
+
+
+
+
+
+
+function a() {
+  return new Promise((resolve, reject) => {
+    if(isError){
+      reject(Error)
+    }
+    
+    setTimeout(()=>{
+      console.log('a')
+      resolve('done')      
+    }, 1000)
+  })
+}
+
+a()
+  .then(res => {
+    console.log(res)
+  })
+  .catch(error => {
+    console.log(error.message)
+  })
+  .finally(() => {
+
+  })
+
+async function asyncFunc () {
+  try {
+    const res = await a()
+    console.log(res)
+  } catch (error) {
+    console.log(error)
+  } finally {
+    console.log('done!!')
+  }
+}
+
+asyncFunc ()
+
+  
