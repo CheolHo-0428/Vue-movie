9e1d343e4f0a14a8cef03e6e394de7809d67ada8
CK-0428 <againnr1@gmail.com>
2021-10-14 16:04:19 +0900

    배포까지 완료

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,14 +1,39 @@
 <template>
-    <div>
-        <ul>
-            <li 
-                v-for="movie in movies"
-                :key="movie.imdbID"
-                >
-                {{ movie }}
-            </li>
-        </ul>
-    </div>
+    <v-row
+        v-masonry
+        item-selector=".item">
+        <!-- cols속성 한줄에 보여줄 갯수(보통은 12개로 지정한다.) -->
+        <!-- lg속성 라지 디바이스에서 12 / 3 = 한줄에 4개씩 보여준다. -->
+        <!-- md속성 미디움 디바이스에서 12 / 3 = 한줄에 4개씩 보여준다. -->
+        <!-- sm속성 스몰 디바이스에서 12 / 6 = 한줄에 2개씩 보여준다. -->
+        <!-- v-masonry는 칸이 맞지 않는 부분을 맞쳐준다. -->
+        <v-col 
+            v-for="movie in movies"
+            :key="movie.imdbID"
+            v-masonry-tile
+            class="item"
+            cols="12"
+            lg="3"  
+            md="3"
+            sm="6">
+            <v-card>
+                <v-img
+                    :src="posterSrc(movie.Poster)"
+                    :alt="movie.Title"
+                    :height="posterHeight(movie.Poster)">
+                    <template v-slot:placeholder>
+                        <div style="background: lightgray; height: 100%"></div>
+                    </template>
+                </v-img>
+                <v-card-title>
+                    {{ movie.Title }}
+                </v-card-title>
+                <v-card-subtitle>
+                    {{ movie.Year }}
+                </v-card-subtitle>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
@@ -16,6 +41,16 @@ export default {
     computed: {
         movies() {
             return this.$store.state.movie.movies
+        }
+    },
+
+    methods: {
+        posterSrc(poster) {
+            return poster === 'N/A' ? '' : poster
+        },
+
+        posterHeight(poster) {
+            return poster === 'N/A' ? 100 : 300
         }
     }
 }
--- src/store/movie.js
+++ src/store/movie.js
@@ -9,29 +9,54 @@ export default {
     movies: []
   }), 
     
-  getters: {
-
-  },
-
   //비동기 처리가 불가능하다. // commit으로 호출한다.
   mutations: {
     updateState(state, payload) {
       Object.keys(payload).forEach(key => {
         state[key] = payload[key]
       })
+    },
+
+    pushIntoMovies(state, movies) {
+      state.movies.push(...movies)
     }
   },
-
+  
   // 비동기 처리가 가능하다. // dispatch로 호출한다.
   actions: {
-    async searchMovies ({ state, commit }) {
+    fetchMovies({state, commit}, pageNum) {
+      return new Promise(async resolve => {
+        const res = await axios.get(`https://www.omdbapi.com/?apikey=c9fcbbb5&s=${state.title}&page=${pageNum}`)
+        commit('pushIntoMovies', res.data.Search)
+        resolve(res.data)
+      })
+    },
+
+    async searchMovies ({ commit, dispatch }) {
       //state.loading = true
       commit('updateState', {
-        loading: true
+        loading: true,
+        movies: []
       })
-      const res = await axios.get(`http://www.omdbapi.com/?apikey=c9fcbbb5&s=${state.title}`)
+      const { totalResults } = await dispatch('fetchMovies', 1)
+      // const res = await axios.get(`http://www.omdbapi.com/?apikey=c9fcbbb5&s=${state.title}&page=1`)
+      // console.log(res)
+      const pageLength = Math.ceil(totalResults / 10)
+      // commit('updateState', {
+      //   movies: res.data.Search,
+      // })
+
+      if (pageLength > 1) {
+        for(let i = 2; i <= pageLength; i+=1){
+          if(i>4) break;
+          //const resMore = await axios.get(`http://www.omdbapi.com/?apikey=c9fcbbb5&s=${state.title}&page=${i}`)
+          //commit('pushIntoMovies', resMore.data.Search)
+          await dispatch('fetchMovies', i)
+        }
+      }
+
       commit('updateState', {
-        movies: res.data.Search,
+        //movies: res.data.Search,
         loading: false
       })
     }
